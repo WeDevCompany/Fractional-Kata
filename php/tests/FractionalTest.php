adeee6bc4f200041b70ba0db4662205895d99dbc
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-11-17 21:23:32 +0100

    Red: a null string should not be valid

--- php/tests/FractionalTest.php
+++ php/tests/FractionalTest.php
@@ -24,6 +24,7 @@ class FractionalTest extends TestCase
      */
     public function createInvalidArguments(): iterator
     {
+        yield [null];
         yield [''];
         yield ['{'];
         yield ['}'];
