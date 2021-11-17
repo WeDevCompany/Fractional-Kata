9efcdb0dcbbdda499fb6cc5a17a56cf4af89cc38
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-11-17 20:36:38 +0100

    creating a parametrizaise test base on the non valid arguments for a fraction

--- php/tests/FractionalTest.php
+++ php/tests/FractionalTest.php
@@ -4,40 +4,43 @@ namespace Fractional\Test;
 
 use Fractional\Fractional;
 use InvalidArgumentException;
+use Iterator;
 use PHPUnit\Framework\TestCase;
 
 class FractionalTest extends TestCase
 {
-
-    /** @test */
-    public function should_not_create_fraction_with_empty_string()
+    /**
+     * @test
+     * @dataProvider createInvalidArguments
+     */
+    public function should_not_create_fraction_with_invalid_input(string $invalidArguments)
     {
         $this->expectException(InvalidArgumentException::class);
-        $invalidFraction = '';
-        Fractional::fractionFromString($invalidFraction);
+        Fractional::fractionFromString($invalidArguments);
     }
 
-    /** @test */
-    public function should_not_create_fraction_with_only_one_bracket()
+    /**
+     * @return iterator<string>
+     */
+    public function createInvalidArguments(): iterator
     {
-        $this->expectException(InvalidArgumentException::class);
-        $invalidFraction = '{';
-        Fractional::fractionFromString($invalidFraction);
-    }
+        yield [''];
+        yield ['{'];
+        yield ['}'];
+        yield ['{}'];
+        yield ['({})'];
+        yield ['({a/a})'];
+        yield ['{a/a}'];
+        yield ['{a3/a3}'];
+        yield ['{3a/3a}'];
+        yield ['{03/3a}'];
+        yield ['{a3/03}'];
+        yield ['{a3/0}'];
+        yield ['{0/0a}'];
+        yield ['{0/a0}'];
+        yield ['{-3/0}'];
+        yield ['{-3/1}']; // TODO: review when this logic should work
+        yield ['{3/-1}']; // TODO: review when this logic should work
 
-    /** @test */
-    public function should_not_create_fraction_with_only_one_inverse_bracket()
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $invalidFraction = '}';
-        Fractional::fractionFromString($invalidFraction);
-    }
-
-    /** @test */
-    public function should_not_create_fraction_with_empty_pair_of_brackets()
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $invalidFraction = '{}';
-        Fractional::fractionFromString($invalidFraction);
     }
 }
