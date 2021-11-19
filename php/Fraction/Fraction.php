f88a7059be65756ceab431f85d755602d7b52c60
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2021-11-19 19:40:41 +0100

    Refactor: numerator

--- php/Fraction/Fraction.php
+++ php/Fraction/Fraction.php
@@ -9,6 +9,9 @@ use InvalidArgumentException;
 class Fraction
 {
     private const VALID_FRACTION_FORMAT = '/{[0-9]+\/[0-9]+}/m';
+    private const FRACTION_ELEMENTS = '/(?:[1-9][0-9]*|0)(?:\/[1-9][0-9]*)?/';
+    private const FRACTION_SEPARATOR = '/';
+    private const NUMERATOR_INDEX = 0;
     private $numerator;
 
     private function __construct(int $numerator)
@@ -20,9 +23,9 @@ class Fraction
     {
         self::throwExceptionIfInvalidFraction($fraction);
         $matches = [];
-        preg_match('/(?:[1-9][0-9]*|0)(?:\/[1-9][0-9]*)?/', $fraction, $matches);
-        $numerator = explode('/', $matches[0]);
-        return new self((int) $numerator[0]);
+        preg_match(self::FRACTION_ELEMENTS, $fraction, $matches);
+        $numerator = self::cleanNumerator($matches[0]);
+        return new self($numerator);
     }
 
     private static function throwExceptionIfInvalidFraction(string $fraction)
@@ -32,9 +35,10 @@ class Fraction
         }
     }
 
-    private static function cleanNumerator(String $fraction)
+    private static function cleanNumerator(string $fraction): int
     {
-
+        $numerator = explode(self::FRACTION_SEPARATOR, $fraction);
+        return (int) $numerator[self::NUMERATOR_INDEX];
     }
 
     public function numerator(): int
